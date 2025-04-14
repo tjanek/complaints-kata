31c021a4aa62155d44c12232bbdc624ab76ee70c
Tomasz Janek <janek.tomasz@gmail.com>
2025-04-14 07:27:40 +0200

    initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # complaints-kata
+
+Create a REST service for managing complaints that allows for:
+ - adding new complaints
+ - editing complaint content
+ - retrieving previously saved complaints
+
+A complaint should include: 
+ - product ID 
+ - content 
+ - creation date 
+ - complainant 
+ - country 
+ - submission count
+
+The "country" field should contain the country from which the client added the complaint,
+based on their IP address (you can use any free service for this). 
+
+Complaints should be unique by product ID and complainant. 
+
+If a duplicate is attempted to be added, the "submission count" field should be incremented without editing other data.
+
+Data should be saved in a database. 
+
+The service should be implemented in Java or Kotlin.
+
+The project should be buildable using Maven or Gradle. 
+
+You can use any readily available technologies (database engines, libraries, frameworks). 
+
+Make sure to follow good programming practices.
---
+++ src/test/groovy/pl/tjanek/complaints/BaseIntegrationSpec.groovy
@@ -0,0 +1,34 @@
+package pl.tjanek.complaints
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import pl.tjanek.complaints.abilities.HttpRequestAbility
+import spock.lang.Specification
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BaseIntegrationSpec extends Specification implements HttpRequestAbility {
+
+    @Value('${local.server.port}')
+    private int port
+
+    @Autowired
+    private TestRestTemplate restTemplate
+
+    ResponseEntity<Map> request(String url, HttpMethod method, Map body = null) {
+        def httpHeaders = new HttpHeaders()
+        httpHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json")
+        httpHeaders.add(HttpHeaders.ACCEPT, "application/json")
+        restTemplate.exchange(
+                "http://localhost:$port$url",
+                method,
+                new HttpEntity<>(body, httpHeaders),
+                Map)
+    }
+
+}
